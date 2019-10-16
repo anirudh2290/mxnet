7eb7ceb54ec2917bd383988dfbdc5c9c8f738405
Anirudh Subramanian <anirudh2290@ufl.edu>
2019-10-16 19:01:09 +0000

    Add cpp-package example

---
+++ cpp-package/example/multithreading_engine_push_test_tls.cpp
@@ -0,0 +1,50 @@
+#include <time.h>
+#include <dmlc/logging.h>
+#include <dmlc/thread_group.h>
+#include <dmlc/omp.h>
+#include <mxnet/c_api.h>
+#include <mxnet/engine.h>
+#include <mxnet/ndarray.h>
+#include <dmlc/timer.h>
+#include <cstdio>
+#include <thread>
+#include <chrono>
+#include <vector>
+
+
+struct A {
+    std::vector<int*> a;
+};
+
+static std::mutex api_lock;
+int ThreadSafetyTest() {
+  std::unique_lock<std::mutex> lock(api_lock);
+  A *ret = dmlc::ThreadLocalStore<A>::Get();
+  std::vector<int*> tmp_inputs;
+  tmp_inputs.reserve(10);
+  for (int i = 0; i < 10; ++i) {
+      tmp_inputs.push_back(new int(i));
+  }
+  ret->a.clear();
+  ret->a.reserve(10);
+  for (int  i = 0; i < 10; ++i) {
+      ret->a.push_back(tmp_inputs[i]);
+  }
+  LOG(INFO) << dmlc::BeginPtr(ret->a);
+}
+
+int main(int argc, char const *argv[]) {
+    auto func = [&](int num) {
+        ThreadSafetyTest();
+        };
+    std::vector<std::thread> worker_threads(5);
+    int count = 0;
+    for (auto&& i : worker_threads) {
+        i = std::thread(func, count);
+        count++;
+    }
+
+    for (auto&& i : worker_threads) {
+        i.join();
+    }
+}
